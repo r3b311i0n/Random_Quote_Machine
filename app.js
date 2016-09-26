682ba7d6bb6ce3014b49be9b009a523cfe22c7ac
r3b311i0n <nasashinega@gmail.com>
2016-09-26 05:30:43 +0530

    final

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const api = "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&key=";
 const twitter = "https://twitter.com/intent/tweet?text=";
 
-var _getQuote = function () {
+var getQuote = function () {
     var key = Math.floor(Math.random() * (999999 - 1 + 1)) + 1;
 
     $.getJSON(api + key, function (quoteJson) {
         $(".quote").html("\"" + quoteJson["quoteText"] + "\"");
-        $(".quoteAuthor").html(" - " + quoteJson["quoteAuthor"]);
+
+        if (quoteJson["quoteAuthor"] != "") {
+            $(".quoteAuthor").html(" - " + quoteJson["quoteAuthor"]);
+        }
+
+        $(".quoteFrame").fadeIn(600);
     });
 };
 
@@ -17,11 +22,13 @@ var tweetQuote = function () {
 };
 
 $("div .newQuoteButton").on("click", function () {
-    _getQuote();
+    $(".quoteFrame").fadeOut(600, getQuote);
 });
 
 $("div .tweetButton").on("click", function () {
     tweetQuote();
 });
 
-$(document).ready(_getQuote());
+$(document).ready(function () {
+    $(".quoteFrame").hide().animate({opacity: 100}, 600, getQuote);
+});
